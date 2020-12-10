25c7db50e08a741baa06cf881e29d9297d6a2eee
Akshay A H <Akshay A H@Akshay>
2020-12-10 11:54:35 +0530

    Controller designed.

--- w2_prob2matfile.m
+++ w2_prob2matfile.m
@@ -2,6 +2,9 @@
 m = 1000;
 b = 50;
 u = 500;
-[A,B,C,D] = linmod('w2_prob3')
-cruise_ss = ss(A,B,C,D);
-step(u*cruise_ss)
+%[A,B,C,D] = linmod('w2_prob3')
+%cruise_ss = ss(A,B,C,D);
+%step(u*cruise_ss)
+Kp = 800;
+Ki = 40;
+r = 10;
